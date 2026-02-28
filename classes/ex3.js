140adcdf37cee8a2e4e688ac3a2b0207d14d13b2
adagga <edgarorina@gmail.com>
2026-02-27 22:41:24 -0700

    nooo

--- classes/ex3.js
+++ classes/ex3.js
@@ -2,14 +2,14 @@
     // add isClassic() > true if year < 2000
 
 class Movie {
-    constructor(title, year, rating) 
+    constructor(title, year, rating)   // sets up the movie definitions 
     {
         this.title = title;
         this.year = year;
         this.rating = rating;
     }
     isClassic() {
-        return this.year < 2000;  // the part that executes the question 
+        return this.year < 2000;  // the part that executes the question; checks the movie's year
     }
 }
 
@@ -17,8 +17,8 @@ const movies = [
     new Movie("Breaking Bad", 2009, 9.6),
     new Movie("Fatal Seduction", 2023, 7.8),
     new Movie("Black Panther", 1999, 6.9)
-];
+];   // created multiple movie instances and stored them in an array 
 
-const classics = movies.filter(m => m.isClassic());
+const classics = movies.filter(m => m.isClassic());   // filtering: calls the method on each object and keeps only the classics
 console.log(classics);
 
